ad4c2bf56b2618a03db622be66ede7ddfaa0082c
JuneCho23 <junecho.ucb@gmail.com>
2017-05-06 16:55:27 -0700

    format

--- AccuWeather/Tests.cs
+++ AccuWeather/Tests.cs
@@ -37,22 +37,17 @@ namespace AccuWeather
 		{
 			app.Tap("button1");
 			app.Screenshot("Let's start by Tapping on the 'Agree' Button");
-
 			app.Tap(x => x.Class("android.widget.ImageButton").Index(0));
-			app.Screenshot("Then we Tapped the 'Hamburger' Button");
 
+			app.Screenshot("Then we Tapped the 'Hamburger' Button");
 			app.Tap("search_text");
 			app.Screenshot("Next we Tapped on the 'Search Text' Field");
-
-
 			app.EnterText("San Francisco");
 			app.Screenshot("We entered our search, 'San Francisco'");
 			app.PressEnter();
-
+			app.Screenshot("Then we Tapped 'Enter' on the keyobard");
 
 			app.Tap(x => x.Class("android.widget.RelativeLayout").Index(1));
-
-
 
 		}
 
